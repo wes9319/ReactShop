b24fdabc4a1ca58ba9166ee27b715548660177ed
Elvis W. Vasquez <wes9319_rhcp@hotmail.com>
2022-04-20 18:09:52 -0500

    Add toggle menu

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/ProductItem.scss';
 import iconAddCart from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = () => {
+
+  const [cart, setCart] = useState([]);
+
+  const handleClick = () => {
+    setCart([]);
+  };
+
   return (
     <div className="ProductItem">
       <img src="https://images.pexels.com/photos/1056251/pexels-photo-1056251.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""/>
@@ -11,7 +18,7 @@ const ProductItem = () => {
           <p>120</p>
           <p>Bike</p>
         </div>
-        <figure>
+        <figure onClick={handleClick}>
           <img src={iconAddCart} alt="Add to cart icon"/>
         </figure>
       </div>
