632b8ae465f023b77e6aa987d48f8cd7b2c4417c
Elvis W. Vasquez <wes9319_rhcp@hotmail.com>
2022-04-21 15:34:40 -0500

    Implement useRef and handler for login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../styles/Login.scss';
 
 const Login = () => {
+
+  const form = useRef(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(form.current);
+    const data = {
+      username: formData.get('email'),
+      password: formData.get('password'),
+    }
+  }
+
   return (
     <div className="Login">
       <div className="Login-container">
         <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo"/>
-        <form action="" className="form">
-          <label for="email">Email address</label>
-          <input type="email" id="email" placeholder="myemail@email.com" className="input input-email"/>
-          <label for="password">Password</label>
-          <input type="password" id="password" placeholder="**********" className="input input-password"/>
-          <input type="submit" value="Log in" className="primary-button login-button"/>
+        <form action="/" className="form" ref={form}>
+          <label htmlFor="email">Email address</label>
+          <input type="email" name="email" placeholder="myemail@email.com" className="input input-email"/>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" placeholder="**********" className="input input-password"/>
+          <button 
+            type="button" 
+            className="primary-button login-button" 
+            onClick={handleSubmit}
+          >
+            Log in
+          </button>
           <a href="/">Forgot my password</a>
         </form>
-        <button className="secondary-button signup-button">Sign up</button>
+        <button className="secondary-button signup-button">
+          Sign up
+        </button>
       </div>
     </div>
   )
